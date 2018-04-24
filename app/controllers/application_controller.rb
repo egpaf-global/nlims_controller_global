1dba7f6c9cb61d52eac4299b959ac46e4f3f030b
GiboProgrammer <giftmalolo@ymail.com>
2018-04-24 13:50:18 +0200

    resolved conflict

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,6 @@
-<<<<<<< HEAD
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
 
-  skip_before_action :verify_authenticity_token  
-  
-  def api_version
-  	
-  	return 1 	
-  end 
-
-  def get_api_documentation(v)
-
-  end
-
-  
-=======
 class ApplicationController < ActionController::API
->>>>>>> 6dca82065ac6ac9c61e5b39195bbe0b4574ba920
+	protect_from_forgery with: :exception
+
+  	skip_before_action :verify_authenticity_token  
 end
