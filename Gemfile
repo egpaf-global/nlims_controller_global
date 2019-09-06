238c284d448660afd57df02462ed048677485afb
Fredrick Chiyenda <fchiyenda@gmail.com>
2019-09-06 11:16:20 +0200

    Added updating for Result Dates to couchdb in LIMs fix script

--- Gemfile
+++ Gemfile
@@ -10,6 +10,9 @@ ruby '2.5.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.5'
+
+gem 'parallel'
+gem 'ruby-progressbar'
 # Use mysql as the database for Active Record
 gem 'mysql2', '>= 0.3.18'
 # Use Puma as the app server
--- bin/fix_lims_data.rb
+++ bin/fix_lims_data.rb
@@ -1,6 +1,11 @@
 @fc_code = YAML.load_file("#{Rails.root}/config/application.yml")['facility_code']
 @couchdb = YAML.load_file("#{Rails.root}/config/couchdb.yml")['development']
 
+@url = @couchdb['protocol'] + '://' + @couchdb['host'] \
+      + ':' + @couchdb['port'].to_s + '/' + @couchdb['prefix'] \
+      + '_' + @couchdb['suffix']
+
+
 puts 'Please enter the path to BHT-EMR-API Format/Default: /var/www/BHT-EMR-API'
 
 @bht_emr_path = gets.chomp
@@ -21,31 +26,45 @@ if acc_num != updated_ac_num
   WHERE accession_number = '#{acc_num}'
 SQL
 else
-  puts "Modification not required"
 end
 
 end
 
 def update_lims(acc_num)
   updated_ac_num = 'X' + @fc_code + acc_num[4..(acc_num.length)]
-  url = @couchdb['protocol'] + '://' + @couchdb['host'] \
-  		+ ':' + @couchdb['port'].to_s + '/' + @couchdb['prefix'] \
-  		+ '_order_' + @couchdb['suffix']
-
   spec = Speciman.find_by(tracking_number: acc_num)
 
   if spec
 	  spec.update(tracking_number: updated_ac_num)
-      couchdb_doc = `curl -XGET #{url}/#{spec.couch_id}`
+      couchdb_doc = `curl -XGET #{@url}/#{spec.couch_id}`
 	  couchdb_doc = JSON.parse(couchdb_doc)
 	  if couchdb_doc['tracking_number'] != updated_ac_num
 		  couchdb_doc['tracking_number'] = updated_ac_num
 		  
-		  response = `curl -XPUT #{url}/#{spec.couch_id} -d '#{couchdb_doc.to_json}'` 
-		  puts "Updated couchdb Record"
+		  response = `curl -XPUT #{@url}/#{spec.couch_id} -d '#{couchdb_doc.to_json}'` 
 	  end
   else
-  	puts "Record not found"
+  end
+end
+
+def update_couchdb_test_result_date(accession_number)
+  results = ActiveRecord::Base.connection.select_all <<~SQL
+    SELECT distinct tracking_number,couch_id,time_entered
+    FROM specimen INNER JOIN tests ON tests.specimen_id = specimen.id
+    INNER JOIN test_results ON test_results.test_id = tests.id
+    INNER JOIN measures ON measures.id = test_results.measure_id
+    WHERE specimen.tracking_number  = '#{accession_number}';
+  SQL
+
+  results.each do |result|
+    couchdb_doc = `curl -XGET #{@url}/#{result['couch_id']}`
+    couchdb_doc = JSON.parse(couchdb_doc)
+    unless couchdb_doc.blank?
+      couchdb_doc['test_results'].each do |key, value|
+            value['date_result_entered'] = result['time_entered']
+            `curl -XPUT #{@url}/#{result['couch_id']} -d '#{couchdb_doc.to_json}'` 
+      end
+    end
   end
 end
 
@@ -65,9 +84,19 @@ ActiveRecord::Base.connection.execute <<~SQL
   ADD INDEX(accession_number);
 SQL
 
-tracking_numbers.each_with_index do |num, i|
-  puts "Processing Record #{i}/#{tracking_numbers.count}"
+Parallel.each(tracking_numbers, progress: 'Processing Sitecode Updates') do |num|
   update_art(num['accession_number'])
   update_lims(num['accession_number'])
 end
 
+ActiveRecord::Base.connection.reconnect!
+
+tracking_numbers = ActiveRecord::Base.connection.select_all <<~SQL
+  SELECT accession_number 
+  FROM #{@art_db['development']['database']}.orders 
+  WHERE accession_number is not NULL;
+SQL
+
+Parallel.each(tracking_numbers, progress: 'Processing Result Date update') do |num|
+   update_couchdb_test_result_date(num['accession_number'])
+end
