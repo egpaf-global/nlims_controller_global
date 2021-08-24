2c8fab75038b66c9cfef7a0ae1434f04361078e0
GiboProgrammer <gmalolo@pedaids.org>
2021-08-24 08:58:09 +0200

    fixed site codes

---
+++ bin/fix_site_code.rb
@@ -0,0 +1,9 @@
+code = "XKCH"
+res = Speciman.find_by_sql("SELECT * FROM specimen WHERE substring(tracking_number,1,4) ='#{code}'")
+res.each do |order|    
+    update = Speciman.find_by(:tracking_number => order.tracking_number)
+    tracking_number_ = order.tracking_number.gsub(code,"XQCH")
+    update.tracking_number = tracking_number_
+    update.save
+end
+puts done
