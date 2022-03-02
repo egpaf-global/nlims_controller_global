d86b734ac71cbe44cf7f3483255c3cd85a5f19be
TMwansambo <https://github.com/TMwansambo/Taxpayer_MRA.git>
2022-03-02 11:46:33 +0200

    added migration files to suite RH4

---
+++ db/migrate/20220302093620_specimen_dispatch_type.rb
@@ -0,0 +1,9 @@
+class SpecimenDispatchType < ActiveRecord::Migration[5.1]
+  def change
+    create_table :specimen_dispatche_types do |t|
+      t.string :name
+      t.string :description
+      t.timestamps
+    end
+  end
+end
