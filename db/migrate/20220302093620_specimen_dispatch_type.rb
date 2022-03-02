154d7c721c2eff77e69c4ee5ee9e8e01f208ef9f
GiboProgrammer <gmalolo@pedaids.org>
2022-03-02 11:49:59 +0200

    added migration files to suite RH4

--- db/migrate/20220302093620_specimen_dispatch_type.rb
+++ db/migrate/20220302093620_specimen_dispatch_type.rb
@@ -3,7 +3,6 @@ class SpecimenDispatchType < ActiveRecord::Migration[5.1]
     create_table :specimen_dispatche_types do |t|
       t.string :name
       t.string :description
-      t.timestamps
     end
   end
 end
