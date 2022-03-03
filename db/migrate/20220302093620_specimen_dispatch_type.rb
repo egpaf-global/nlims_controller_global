20d94f2ad9aeee8c166ed598928f843da1bf2771
GiboProgrammer <gmalolo@pedaids.org>
2022-03-03 07:34:29 +0200

    removed migration file

--- db/migrate/20220302093620_specimen_dispatch_type.rb
+++
@@ -1,8 +0,0 @@
-class SpecimenDispatchType < ActiveRecord::Migration[5.1]
-  def change
-    create_table :specimen_dispatche_types do |t|
-      t.string :name
-      t.string :description
-    end
-  end
-end
