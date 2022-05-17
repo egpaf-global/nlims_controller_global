c5eb2162e184905f2eb1b0c8bf7a55b8c1fe8789
GiboProgrammer <gmalolo@pedaids.org>
2022-05-17 09:49:07 +0200

    initial

--- lib/test_service.rb
+++ lib/test_service.rb
@@ -102,7 +102,7 @@ module TestService
 						
 						measure = Measure.where(name: measure_name).first
                         next if measure.blank?      
-						if check_if_result_already_available(test_id,measure_id) == false                  
+						if check_if_result_already_available(test_id,measure.id) == false                  
 							TestResult.create(
 								measure_id: measure.id,
 								test_id: test_id,
@@ -114,21 +114,18 @@ module TestService
 						test_results_measures[measure_name] = { 'result_value': result_value }						
 					end	
 					results_measure[test_name] = test_results_measures	
-					if params[:test_status] != "completed" &&  params[:test_status] != "verified"
-						test_status = TestStatus.where(name: "completed").first
-					else
-						test_status = TestStatus.where(name: params[:test_status]).first
-					end				
+
+						test_status = TestStatus.where(name: params[:test_status]).first			
 						tst_update = Test.find_by(:id => test_id)
 						tst_update.test_status_id = test_status.id
 						tst_update.save
-					end
+					
 				else
-					if params[:test_status] != "completed" && params[:test_status] != "verified"
+							test_status = TestStatus.where(name: params[:test_status]).first
 						tst_update = Test.find_by(:id => test_id)
 						tst_update.test_status_id = test_status.id
 						tst_update.save
-					end
+
 				end		
 				
 				if !results_measure.blank?
