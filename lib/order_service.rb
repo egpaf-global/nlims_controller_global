ba926387de2db150b5ad563f0ab4e0a71e7caeff
hopgausi <hopgausi20@gmail.com>
2022-10-19 15:34:12 +0200

    Fix sites with special chars

--- lib/order_service.rb
+++ lib/order_service.rb
@@ -540,7 +540,7 @@ module  OrderService
                                                       INNER JOIN tests ON tests.specimen_id = specimen.id
                                                       INNER JOIN patients ON patients.id = tests.patient_id
                                                       LEFT JOIN wards ON specimen.ward_id = wards.id
-                                                      WHERE specimen.tracking_number NOT IN (SELECT tracking_number FROM specimen_dispatches)")
+                                                      WHERE specimen.sending_facility ='#{res[0]['site_name'].gsub("'", "\\\\'")}' AND specimen.tracking_number NOT IN (SELECT tracking_number FROM specimen_dispatches)")
                                                       tsts = {}
                         
                         if res_.length > 0
